49bf3de54a1f5eb3fd19b2815031da682d0e5423
XCader <100986647+luacoder279@users.noreply.github.com>
2024-01-18 21:29:36 +0000

    Changes

--- FusionUtil/FusionUtil/FusionCore.cs
+++ FusionUtil/FusionUtil/FusionCore.cs
@@ -3,6 +3,10 @@ namespace FusionUtil
 {
     public class FusionCore
     {
+      public static void Main()
+      {
+        InitAPI();
+      }
         public static void InitAPI()
         {
           Console.WriteLine("Fusion Utility System API Initalized");
